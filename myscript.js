491962aa8939ed9b48210c323ca3be6bff7d12dc
bency <bency80097@gmail.com>
2015-04-30 01:24:53 +0800

    使用原生演算法在本地計算圖片 url

--- myscript.js
+++ myscript.js
@@ -1,4 +1,3 @@
-var data;
 function extLocalStorage (namespace){
     var localStorage = window.localStorage || {};
     if(typeof namespace !== "string") {
@@ -21,6 +20,31 @@ function extLocalStorage (namespace){
     };
     return mainFunction;
 };
+
+var request = function (queryStringName) {
+    var returnValue = "";
+    var URLString = new String(document.location);
+    var serachLocation = -1;
+    var queryStringLength = queryStringName.length;
+    do {
+        serachLocation = URLString.indexOf(queryStringName + "\=");
+        if (serachLocation != -1) {
+            if ((URLString.charAt(serachLocation - 1) == '?') || (URLString.charAt(serachLocation - 1) == '&')) {
+                URLString = URLString.substr(serachLocation);
+                break;
+            }
+            URLString = URLString.substr(serachLocation + queryStringLength + 1);
+        }
+    } while (serachLocation != -1) if (serachLocation != -1) {
+        var seperatorLocation = URLString.indexOf("&");
+        if (seperatorLocation == -1) {
+            returnValue = URLString.substr(queryStringLength + 1);
+        } else {
+            returnValue = URLString.substring(queryStringLength + 1, seperatorLocation);
+        }
+    }
+    return returnValue;
+}
 
 
 function getRandomToken () {
@@ -66,59 +90,107 @@ function htmlEncode (value){
     return $('<div/>').text(value).html();
 }
 
+// 撈 comic hash
+var $scripts = $('script');
+for (var i = 0; i < $scripts.length; i++) {
+    if ($scripts[i].innerHTML.match(/var cs=/)) {
+        var $script = $scripts[i];
+        cs  = $script.innerHTML.match(/var cs='([\w]*)'/)[1] || null;
+        ti  = $script.innerHTML.match(/var ti=([\d]*);/)[1] || null;
+        chs = $script.innerHTML.match(/var chs=([\w]*);/)[1] || null;
+    }
+}
+
+/* cs: 很長的那串 hash
+ * ti: http://new.comicvip.com/show/cool-103.html?ch=783 裡面的 103
+ * chs: 最新話數
+ */
+function Vol (cs, ti, page) {
+    var comicHash = cs;
+    var ch = request('ch');
+    var vol_id = ti;
+    var factor = 50;
+    var max_ch = comicHash.length / factor - 1;
+    var page = page;
+    var pad_zero = function (n) {
+        return n < 10 ? '00' + n : n < 100 ? '0' + n : n;
+    }
+    var ss = function (a, b, c, d) {
+        var e = a.substring(b, b + c);
+        return d == null ? e.replace(/[a-z]*/gi, "") : e;
+    }
+    var mm = function (p) {
+        return (parseInt((p - 1) / 10) % 10) + (((p - 1) % 10) * 3)
+    };
+    if ('undefined' === typeof page) {
+        console.log(ch);
+        if (ch.indexOf("-") > 0) {
+            page = parseInt(ch.split('-')[1]);
+            ch = ch.split('-')[0];
+        } else {
+            page = 1
+        }
+    }
+
+    var getVolHash = function () {
+        var cc = comicHash.length;
+        for (var i = 0; i < cc / factor; i++) {
+            if (ss(comicHash, i * factor, 4) == ch) {
+                return ss(comicHash, i * factor, factor, factor);
+            }
+        }
+        return ss(comicHash, cc - factor, factor);
+    }
+    this.volHash = getVolHash();
+    var maxPage = ss(this.volHash, 7, 3);
+    this.getPicUrl = function () {
+        var url = 'http://img' + ss(this.volHash, 4, 2) + '.8comic.com/' + ss(this.volHash, 6, 1) + '/' + vol_id + '/' + ss(this.volHash, 0, 4) + '/' + pad_zero(page) + '_' + ss(this.volHash, mm(page) + 10, 3, factor) + '.jpg';
+        if (page == maxPage && ch == max_ch) {
+            return '#';
+        } else if (page == maxPage) {
+            page = 1;
+            ch++;
+        } else {
+            page++;
+        }
+        return url;
+    }
+    this.getUrlPostfix = function () {
+        return ch + '-' + (page - 1);
+    }
+}
+var vol = new Vol(cs, ti);
+console.log(vol);
+$('#TheTable > tbody > tr > td').append('<img src="' + vol.getPicUrl() + '"><hr>');
+$('#TheTable > tbody > tr > td').append('<img src="' + vol.getPicUrl() + '"><hr>');
+$('#TheTable > tbody > tr > td').append('<img src="' + vol.getPicUrl() + '"><hr>');
+
 var loadPic = function() {
     // 畫面上緣
     var wtop = $(window).scrollTop();
 
     // 漫畫底部
     var btop = $('#TheTable > tbody > tr > td').height() + $('#TheTable > tbody > tr > td').offset().top;
-    if ((loaded == 0 || btop - wtop < window.innerHeight * 1.2)  && data.total > 0) {
-        $('img:hidden').first().fadeIn(1000);
-        if (loaded < data.total) {
-            if (data.urls.length > 0 && $('img:hidden').length == 0) {
-                $('#TheTable > tbody > tr > td').append('<img style="display:none;margin-top:30px;" src="' + data.urls.pop() + '"><hr>');
-                loaded++;
-            }
-        } else {
-            $(window).unbind('scroll', loadPic);
-            path = location.href.split('=')[0];
-            vol = parseInt(location.href.split('=')[1]) + 1;
-            new_url = path + '=' + vol;
-            console.log(new_url);
-            history.pushState({}, null, new_url);
-            getHtml(new_url);
-        }
+    if ((btop - wtop > window.innerHeight * 1.2)) {
+        console.log(btop - wtop, window.innerHeight * 1.2);
+        return;
+    }
+    $('img:hidden').first().fadeIn(1000);
+    if ($('img:hidden').length < 2) {
+        $('#TheTable > tbody > tr > td').append('<img style="display:none;margin-top:30px;" src="' + vol.getPicUrl() + '"><hr>');
+        path = location.href.split('=')[0];
+        new_url = path + '=' + vol.getUrlPostfix();
+        console.log(new_url);
+        history.pushState({}, null, new_url);
     }
 }
+$(window).on('scroll', loadPic);
 
-function scrollInit (ret) {
-    if (ret) {
-        data = ret;
-        data.urls.reverse();
-        for (i=0; i < 3; i++) {
-            $('#TheTable > tbody > tr > td').append('<img src="' + data.urls.pop() + '"><hr>');
-        }
-        loaded = 3;
-        $(window).on('scroll', loadPic);
-    }
-}
-
-function getHtml (url) {
-    $('#loading-fa').addClass('loading-fa');
-    loaded = 0;
-    $.ajax({
-        url: 'http://infinite.bency.org/get_8comic.php',
-        dataType: 'json',
-        data: {url: url, method: 'json'},
-        //jsonp: 'callback',
-        //jsonpCallback: 'showpic',
-        success: scrollInit
-    });
-}
+// 去廣告
 $('#TheImg').remove();
 $('#Form1').ready(function(){
     $('#Form1 > table:nth-child(3)').remove();
     $('#Form1 > table:nth-child(3)').remove();
     $('iframe').remove();
 });
-getHtml(location.href);
+// 去廣告結束
