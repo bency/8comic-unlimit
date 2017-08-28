5539615f157a1a721548d4ede0303893a1f03402
bency <bency@pixnet.tw>
2017-08-29 03:45:21 +0800

    cs ti chs 改為 protected variable

--- bootstrap.js
+++ bootstrap.js
@@ -2,23 +2,23 @@ class Bootstrap {
 
     constructor() {
         this.y = 46;
+        this.cs = '';
+        this.ti = '';
+        this.chs = '';
     }
 
     getBasicParams(html) {
-        let cs = '',
-            ti = '',
-            chs = '';
         // 撈 comic hash
         let $scripts = $(html).find('script');
         for (let i = 0; i < $scripts.length; i++) {
             if ($scripts[i].innerHTML.match(/var cs=/)) {
                 let $script = $scripts[i];
-                cs  = $script.innerHTML.match(/var cs='([\w]*)'/)[1] || null;
-                ti  = parseInt($script.innerHTML.match(/var ti=([\d]*);/)[1]) || null;
-                chs = parseInt($script.innerHTML.match(/var chs=([\w]*);/)[1]) || null;
+                this.cs  = $script.innerHTML.match(/var cs='([\w]*)'/)[1] || null;
+                this.ti  = parseInt($script.innerHTML.match(/var ti=([\d]*);/)[1]) || null;
+                this.chs = parseInt($script.innerHTML.match(/var chs=([\w]*);/)[1]) || null;
             }
         }
-        return {cs, ti, chs};
+        return {cs: this.cs, ti: this.ti, chs: this.chs};
     }
 
     request (queryStringName) {
