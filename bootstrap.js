cfefaac4ce45f812069e6057c27626d928f872ab
bency <bency@pixnet.tw>
2017-08-29 03:03:23 +0800

    add function request

--- bootstrap.js
+++ bootstrap.js
@@ -20,5 +20,30 @@ class Bootstrap {
         }
         return {cs, ti, chs};
     }
+
+    request (queryStringName) {
+        var returnValue = "";
+        var URLString = new String(document.location);
+        var serachLocation = -1;
+        var queryStringLength = queryStringName.length;
+        do {
+            serachLocation = URLString.indexOf(queryStringName + "\=");
+            if (serachLocation != -1) {
+                if ((URLString.charAt(serachLocation - 1) == '?') || (URLString.charAt(serachLocation - 1) == '&')) {
+                    URLString = URLString.substr(serachLocation);
+                    break;
+                }
+                URLString = URLString.substr(serachLocation + queryStringLength + 1);
+            }
+        } while (serachLocation != -1) if (serachLocation != -1) {
+            var seperatorLocation = URLString.indexOf("&");
+            if (seperatorLocation == -1) {
+                returnValue = URLString.substr(queryStringLength + 1);
+            } else {
+                returnValue = URLString.substring(queryStringLength + 1, seperatorLocation);
+            }
+        }
+        return returnValue;
+    }
 }
 module.exports = Bootstrap;
